f8e22c5311331e6e863b5b833f88d59c167d36f2
Steven Li <steven4354@gmail.com>
2018-01-29 13:54:47 -0800

    created testing files

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import TestCompContainer from './containers/TestCompContainer';
-import About from './components/About/About';
+import React from "react";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+//custom components and containers
+import TestCompContainer from "./containers/TestCompContainer";
+import About from "./components/About/About";
+import UserForm from "./components/Login/UserForm";
+
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const Routes = () => {
   return (
     <Router>
       <div>
+        <Route exact path="/login" component={UserForm} />
         <Route exact path="/test" component={TestCompContainer} />
         <Route exact path="/about" component={About} />
       </div>
     </Router>
-  )
+  );
 };
 
 export default Routes;
