c19f24284669b817bc98d96b0e3345001b1fa60d
Steven Li <steven4354@gmail.com>
2018-01-30 08:07:42 -0800

    created json web tokens

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import MasterContainer from "./components/Project/MasterContainer";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Landing from "./components/Landing/Landing.js";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const Routes = () => {
   return (
@@ -19,12 +19,8 @@ const Routes = () => {
         <Route exact path="/login" component={UserForm} />
         <Route exact path="/test" component={TestCompContainer} />
         <Route exact path="/about" component={About} />
-<<<<<<< HEAD
-        {/* <Route exact path="/rmarkdown" component={Rmarkdown} /> */}
-=======
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/landing" component={Landing} />
->>>>>>> 7d2006518ca57248bc958996c90eb6889804c4eb
       </div>
     </Router>
   );
