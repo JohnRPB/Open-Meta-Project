e5159e2e6fc386004c8063bc44455638833a4646
EdwinYung <eyung7@gmail.com>
2018-01-29 19:52:59 -0500

    draft of project page

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ import React from "react";
 import TestCompContainer from "./containers/TestCompContainer";
 import About from "./components/About/About";
 import UserForm from "./components/Login/UserForm";
+import MasterContainer from "./components/Project/MasterContainer";
 
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Routes = () => {
   return (
     <Router>
       <div>
+        <Route exact path="/project" component={MasterContainer} />
         <Route exact path="/login" component={UserForm} />
         <Route exact path="/test" component={TestCompContainer} />
         <Route exact path="/about" component={About} />
