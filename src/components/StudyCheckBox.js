12528d9d320a199332ab9f7cf1ac9d79d2c6b836
Johann <baptistajohann@gmail.com>
2018-02-02 07:34:31 -0800

    mcomp added

---
+++ src/components/StudyCheckBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Link,
+  Switch,
+} from 'react-router-dom'
+
+const StudyCheckBox = () => {
+  
+  return (
+  
+  );
+}
+
+export default StudyCheckBox;
