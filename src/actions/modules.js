801ddf17cfcd4c948d50c4117d53c57e1f734721
Johann <baptistajohann@gmail.com>
2018-02-02 10:03:25 -0800

    something

--- src/actions/modules.js
+++ src/actions/modules.js
@@ -1,20 +1,26 @@
 
-export const UPDATE_STUDIES = 'UPDATE_STUDIES';
-export const updateStudies = (studies) => {
-  return {type: UPDATE_STUDIES, data: };
-};
-export const DB_TEST_RESULTS = 'DB_TEST_RESULTS';
-export const dbTestResults = data => {
-  return {type: DB_TEST_RESULTS, data};
-};
-export const DB_TEST_ERROR = 'DB_TEST_ERROR';
-export const dbTestError = data => {
-  return {type: DB_TEST_ERROR, data};
+// Remove a study
+export const REMOVE_STUDY = 'REMOVE_STUDY';
+export const removeStudy = (studyId) => {
+  return {type: REMOVE_STUDY, data: studyId };
 };
 
-export const DB_TEST = 'DB_TEST';
-export const dbTest = () => {
+// Add a study
+export const ADD_STUDY = 'ADD_STUDY';
+export const addStudy = (studyId) => {
+  return {type: ADD_STUDY, data: studyId };
+};
+
+export const UPDATE_SINGLE_STUDY = 'UPDATE_SINGLE_STUDY';
+export const updateSingleStudy = (updateType, studyId) => {
   return dispatch => {
+
+    updateType ? 
+      addStudy(studyId) : 
+      removeStudy(studyId);
+
+    
+
     dispatch(dbTestStart());
     axios
       .get(`${root}/api/users`)
