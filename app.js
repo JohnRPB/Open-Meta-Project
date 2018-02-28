d9e11ea057485a652bfb3e7e6a54eae0ebd9a9c6
GeneTinderholm <tinderholmgene@gmail.com>
2018-02-28 12:41:23 -0600

    heroku test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morganToolkit = require('morgan-toolkit')(morgan);
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const api = require('./api');
+const api = require('./api/server/api.js');
 
 // Setup logger
 
@@ -15,11 +15,12 @@ const api = require('./api');
 // app.use((req, res, next) =>
 const mongoose = require('mongoose');
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use((req, res, next) => {
   if (mongoose.connection.readyState) {
     next();
   } else {
-    require('../mongo')().then(() => next());
+    require('./api/mongo.js')().then(() => next());
   }
 });
 // parse application/x-www-form-urlencoded
@@ -28,12 +29,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/api', api);
-
 // Serve static assets
-app.use(require('./tokenVerify'));
-app.use('/', (req,res,next) => res.send('Works'));
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(require('./api/server/tokenVerify.js'));
+// app.use('/', (req,res,next) => res.send('Works'));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
 // Serve our api
+
+app.use('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
